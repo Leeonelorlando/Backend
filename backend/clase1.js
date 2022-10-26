9494dc475c1aad18c2ebcfdddfc4fae73a57d518
Leonel Orlando <leonelorlando98@hotmail.com>
2022-10-26 20:06:09 -0300

    first commit

---
+++ backend/clase1.js
@@ -0,0 +1,35 @@
+const nombre = "Leonel"
+
+let edad = "24"
+
+let precio = 99.90
+
+const seriesFavoritas = ["Vikings, 3%, Breaking Bad"]
+
+peliculasFavoritas = [
+    {
+        nombre: "Million Dollar Baby",
+        estreno: 2005,
+        protagonistas: ["Hilary Swank, Clint Eastwood, Morgan Freeman"]
+    },
+    {
+        nombre: "You're Not You",
+        estreno: 2014,
+        protagonistas: ["Hilary Swank, Emmy Rossum, Josh Duhamel"]
+    },
+]
+
+console.log("Nombre")
+console.log(nombre)
+
+console.log("Edad")
+console.log(edad)
+
+console.log("Precio")
+console.log("$" + precio)
+
+console.log("Series Favoritas")
+console.log(seriesFavoritas)
+
+console.log("Peliculas Favoritas")
+console.log(peliculasFavoritas)
